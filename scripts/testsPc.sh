2b84a9df9bb36ea27abc09722fe8e8e7aba3a6b4
rgarciade <garcia.de.la.fuente.raul@gmail.com>
2021-06-06 22:29:56 +0200

    tests pc sh

--- scripts/testsPc.sh
+++ scripts/testsPc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "ejecutando migraciones"
+echo "ejecutando migraciones......"
 ./vendor/bin/sail artisan migrate:fresh >> logs/migrate.txt
-echo "migraciones listas"
-echo "ejecutando seed"
+echo "-migraciones listas"
+echo "ejecutando seed......"
 ./vendor/bin/sail artisan db:seed >> logs/seed.txt
-echo "seed creados"
+echo "-seed creados"
 ./vendor/bin/sail artisan test
