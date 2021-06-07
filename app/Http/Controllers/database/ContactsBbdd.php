36068117806fba2e5fd8ef7b36819427d4ae181f
rgarciade <garcia.de.la.fuente.raul@gmail.com>
2021-06-07 23:52:05 +0200

    contact database

--- app/Http/Controllers/database/ContactsBbdd.php
+++ app/Http/Controllers/database/ContactsBbdd.php
@@ -4,7 +4,5 @@ namespace App\Http\Controllers\database;
 use App\Models\Contacts;
 
 class ContactsBbdd extends commonBbdd {
-    function __construct() {
-        $this->model = new Contacts();
-    }
+    protected static $model = Contacts::class;
 }
