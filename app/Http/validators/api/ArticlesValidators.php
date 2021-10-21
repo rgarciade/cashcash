79e9dc04c6f2dedf47296c407ddcbcf290111d43
“rgarciade” <raulgarcia_dlf@hotmail.com>
2021-10-21 19:27:50 +0200

    test

--- app/Http/validators/api/ArticlesValidators.php
+++ app/Http/validators/api/ArticlesValidators.php
@@ -10,8 +10,8 @@ class ArticlesValidators extends Validator {
             'productid' => 'required|min:1|max:20',
             'description' => 'required|min:5|max:255',
             'units' => 'required|min:1|max:20',
-            'purchase_price' => 'required|min:1|max:10',
-            'public_price' => 'required|min:1|max:10'
+            'public_price' => 'required|min:1|max:10',
+            'purchase_price' => 'required|min:1|max:10'
         ]);
     }
     public static function verifyUpdateArticles(Request $request){
